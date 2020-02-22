654030bf3f7336d9fa71242240cc1ebe6175765f
SlavonCLK <skandik_y@yahoo.com>
2020-02-22 10:54:54 +0200

    Initial

---
+++ start.py
@@ -0,0 +1,15 @@
+import math
+import os
+import sys
+
+#print(sys.version)
+#print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(sys.executable)
+print(greet("Corey2"))
